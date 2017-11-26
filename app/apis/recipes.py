aa5748459e5b0530a850d4db00cc92a139184a69
Thegaijin <semaganda@outlook.com>
2017-11-26 11:19:28 +0300

    Add namespace and data formats for recipe and category for the structure of the response.

--- app/apis/recipes.py
+++ app/apis/recipes.py
@@ -4,3 +4,24 @@
 # Local imports
 from app.models.category import Category
 from app.models.recipe import Recipe
+
+# Third party imports
+from flask import jsonify, make_response, request
+from flask_restplus import fields, Namespace, Resource, reqparse
+
+
+api = Namespace(
+    'recipes', description='Creating, viewing, editing and deleting recipes and\
+    recipe categories')
+
+category = api.model('Category', {
+    'name': fields.String(required=True, description='category name'),
+    'description': fields.String(required=True,
+                                 description='category description'),
+})
+
+recipe = api.model('Recipe', {
+    'name': fields.String(required=True, description='Recipe name'),
+    'description': fields.String(required=True,
+                                 description='Recipe description'),
+})
