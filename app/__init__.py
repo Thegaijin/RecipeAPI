e9a67b4539a06c6f403272e5249a40bee8a47ba1
Thegaijin <semaganda@outlook.com>
2017-11-26 11:15:33 +0300

    Move imports to the top to solve circular imports issue

--- app/__init__.py
+++ app/__init__.py
@@ -13,28 +13,29 @@ from flask_sqlalchemy import SQLAlchemy
 from instance.config import app_config
 from app.apis import apiv1_blueprint as api_v1
 
+# object to interact with the Database
+db = SQLAlchemy()
+
 
 def create_app(config_name):
-    ''' Creates an instance of the FlaskAPI class
+    ''' Creates an instance of the Flask class
         Loads configuration settings and connects to the required DB
+        Registers the blueprint with the namespaces
         returns the instance
     '''
 
     # Load configurations from instance folder
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    # object to interact with the Database
-    db = SQLAlchemy(app)
     # Updates the values from the given object
     app.config.from_object(app_config[config_name])
     # Updates the values in the config from a Python file
     app.config.from_pyfile('config.py')
     # set to False to avoid wasting resources
-
-    app.register_blueprint(api_v1, url_prefix='/api/v1')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # prep application to work with SQLAlchemy
     db.init_app(app)
+
+    app.register_blueprint(api_v1, url_prefix='/api/v1')
 
     return app
--- app/apis/__init__.py
+++ app/apis/__init__.py
@@ -5,20 +5,19 @@
 from flask import Blueprint
 from flask_restplus import Api
 
+# Local imports
+from .auth import api as ns_auth
+from .recipes import api as ns_recipes
+
 apiv1_blueprint = Blueprint('api_v1', __name__)
 
 api = Api(apiv1_blueprint,
           title='Recipes API',
           version='1.0',
           description='An API to create, read, update and delete recipes')
+
 # namespace for user registration and login
-from .auth import api as ns_auth
 api.add_namespace(ns_auth)
 
 # namespace for the recipes CRUD
-from .recipes import api as ns_recipes
-api.add_namespace(ns_recipes)
-
-
-api.add_namespace(ns_auth)
 api.add_namespace(ns_recipes)
