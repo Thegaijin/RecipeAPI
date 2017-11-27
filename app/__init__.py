c1c4be5860eb3444279ff971fc5e47bc7812e6d9
Thegaijin <semaganda@outlook.com>
2017-11-27 12:21:55 +0300

    Move the url prefix to the blueprint instance

--- app/__init__.py
+++ app/__init__.py
@@ -36,6 +36,6 @@ def create_app(config_name):
     # prep application to work with SQLAlchemy
     db.init_app(app)
     from app.apis import apiv1_blueprint as api_v1
-    app.register_blueprint(api_v1, url_prefix='/api/v1')
+    app.register_blueprint(api_v1)
 
     return app
