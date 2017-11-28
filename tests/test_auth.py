3ba15b77c07d6844742859b77a21df931dfe859e
Thegaijin <semaganda@outlook.com>
2017-11-28 23:05:33 +0300

    Separate the test classes

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,40 +1,12 @@
 # test_recipes.py
-''' This Script has the tests for the application functionality '''
+''' This Script has the tests for user registration and login functionality '''
 
 # Third party imports
 import json
 from unittest import TestCase
 
-# Local imports
-from app import create_app, db
-
-
-class BaseTestCase(TestCase):
-    ''' Setup the shared testing settings '''
-
-    def setUp(self):
-
-        # set app to use testing configuration, initialising the test_yummy db
-        self.app = create_app(config_name="testing")
-
-        # Client object to send virtual requests to the application
-        self.client = self.app.test_client
-
-        # The user details json data to be used for testing
-        self.user = {'username': 'username',
-                     'password': 'password'}
-
-        # explicitly create application context
-        # cache DB connections to be created on a per-request or usage case
-        with self.app.app_context():
-            # current_app points to app
-            db.create_all()
-
-    def tearDown(self):
-        with self.app.app_context():
-
-            db.session.close()
-            db.drop_all()
+# Local import
+from tests.test_base import BaseTestCase
 
 
 class UserTestCase(BaseTestCase):
---
+++ tests/test_recipes.py
@@ -0,0 +1,14 @@
+# test_recipes.py
+''' This Script has the tests for CRUD functionality '''
+
+# Third party imports
+import json
+from unittest import TestCase
+
+# Local import
+from tests.test_base import BaseTestCase
+
+
+class RecipeTestCase(BaseTestCase):
+    ''' Tests for Category and recipe CRUD '''
+    pass
