fdbc4fb1c786a7ac6300edfeaa3f804b1853077a
Thegaijin <semaganda@outlook.com>
2017-12-01 22:23:48 +0300

    Add CRUD functionality (incomplete)

--- app/apis/recipes.py
+++ app/apis/recipes.py
@@ -10,6 +10,7 @@ import traceback
 # Third party imports
 from flask import request
 from flask_restplus import fields, Namespace, Resource, reqparse
+from flask_jwt import jwt_required, current_identity
 
 
 api = Namespace(
@@ -36,12 +37,36 @@ parser.add_argument('description', required=True)
 
 
 @api.route('/categories/')
-@api.route('/categories/<id>/')
+@api.route('/categories/<name>/')
 class Categories(Resource):
     ''' The class handles the Category CRUD functionality '''
 
+    @api.response(200, 'Category found successfully')
+    def get(self, name):
+        ''' This method returns a category '''
+        try:
+            the_category = Category.query.filter_by(name=name).first()
+            if the_category is not None:
+
+                ''' return jsonify(id=the_category.id,
+                               name=the_category.name,
+                               description=the_category.description), 200 '''
+                get_response = {}
+                get_response['id'] = the_category.id
+                get_response['name'] = the_category.name
+                get_response['description'] = the_category.description
+                return get_response, 200
+
+        except Exception as e:
+            get_response = {
+                'message': str(e)
+            }
+
+            return get_response, 404
+
     # specifies the expected input fields
     @api.expect(parser)
+    @api.response(201, 'Category created successfully')
     def post(self):
         ''' This method adds a new category to the DB
 
@@ -49,33 +74,236 @@ class Categories(Resource):
         :param description: string: The category description
         :return: A dictionary with a message
         '''
-        # Get the access token from the header
-        auth_header = request.headers.get('Authorization')
-        return auth_header
-        # get token from header (Bearer <token>)
-        ''' access_token = auth_header.split(" ")[1]
-
-        if access_token:
-            # Get user id from token
-            user_id = User.decode_token(access_token)
-
         args = parser.parse_args()
         name = args.name
         description = args.description
-        created_by = user_id
+        # created_by = user_id
 
         try:
             # check if the category exists
-            if User.query.filter_by(name=name).first() is not None:
-                category = Category(name, description, created_by)
+            if Category.query.filter_by(name=name).first() is None:
+                category = Category(name, description)  # add created_by
                 db.session.add(category)
                 db.session.commit()
                 the_response = {
-                    'status': 'Successfully creation',
+                    'status': 'Success',
                     'message': 'Category was successfully created'
                 }
-            taceback.print_exc()
+
+                return the_response, 201
             return {'message': 'Category already exists'}
-        except:
-            pass
- '''
+        except Exception as e:
+
+            post_response = {
+                'message': str(e)
+            }
+            traceback.print_exc()
+            return post_response
+
+    @api.expect(parser)
+    @api.response(204, 'Successfully edited')
+    def put(self, name=None, description=None):
+        ''' This method edits a category '''
+        try:
+            the_category = Category.query.filter_by(name=name).first()
+            if the_category is not None:
+                args = parser.parse_args()
+                name = args.name
+                description = args.description
+
+                if name is not None and description is not None:
+                    the_category.name = name
+                    the_category.description = description
+                    db.session.add(the_category)
+                    db.session.commit()
+                    edit_response = {
+                        'status': 'Success',
+                        'message': 'Category details successfully edited'
+                    }
+
+                    return edit_response, 204
+                elif name is not None and description is None:
+                    the_category.name = name
+                    db.session.add(the_category)
+                    db.session.commit()
+                    edit_response = {
+                        'status': 'Success',
+                        'message': 'Category name successfully edited'
+                    }
+
+                    return edit_response, 204
+                elif name is None and description is not None:
+                    the_category.description = description
+                    db.session.add(the_category)
+                    db.session.commit()
+                    edit_response = {
+                        'status': 'Success',
+                        'message': 'Category description successfully edited'
+                    }
+
+                    return edit_response, 204
+                else:
+                    return {'message': 'No changes to be made'}
+                return {'message': 'The category does not exist'}
+        except Exception as e:
+
+            edit_response = {
+                'message': str(e)
+            }
+            traceback.print_exc()
+            return post_response
+
+    @api.response(204, 'Success')
+    def delete(self, name):
+        ''' This method deletes a Category '''
+        try:
+            the_category = Category.query.filter_by(name=name).first()
+            if the_category is not None:
+                db.session.delete(the_category)
+                db.session.commit()
+                return {'message': 'The category was successfully deleted'}
+            else:
+                return {'message': 'The category does not exist'}
+        except Exception as e:
+
+            post_response = {
+                'message': str(e)
+            }
+            traceback.print_exc()
+            return post_response
+
+########################## RECIPES ############################
+
+
+@api.route('/recipes/')
+@api.route('/recipes/<name>/')
+class Recipes(Resource):
+    ''' The class handles the Category CRUD functionality '''
+
+    @api.response(200, 'Success')
+    def get(self, name):
+        ''' This method returns a category '''
+        try:
+            the_recipe = Recipe.query.filter_by(name=name).first()
+            if the_recipe is not None:
+
+                get_response = {}
+                get_response['id'] = the_recipe.id
+                get_response['name'] = the_recipe.name
+                get_response['description'] = the_recipe.description
+                return get_response, 200
+
+        except Exception as e:
+            get_response = {
+                'message': str(e)
+            }
+
+            return get_response, 404
+
+    # specifies the expected input fields
+    @api.expect(parser)
+    @api.response(201, 'Success')
+    def post(self):
+        ''' This method adds a new recipe to the DB
+
+        :param name:A string: The recipe name
+        :param description: string: The recipe description
+        :return: A dictionary with a message
+        '''
+        args = parser.parse_args()
+        name = args.name
+        description = args.description
+        # created_by = user_id
+
+        try:
+            # check if the category exists
+            if Recipe.query.filter_by(name=name).first() is None:
+                recipe = Recipe(name, description)  # add created_by
+                db.session.add(recipe)
+                db.session.commit()
+                the_response = {
+                    'status': 'Success',
+                    'message': 'Recipe was successfully created'
+                }
+
+                return the_response, 201
+            return {'message': 'Recipe already exists'}
+        except Exception as e:
+
+            post_response = {
+                'message': str(e)
+            }
+            traceback.print_exc()
+            return post_response
+
+    @api.expect(parser)
+    @api.response(204, 'Success')
+    def put(self, name=None, description=None):
+        ''' This method edits a recipe '''
+        try:
+            the_recipe = Recipe.query.filter_by(name=name).first()
+            if the_recipe is not None:
+                args = parser.parse_args()
+                name = args.name
+                description = args.description
+
+                if name is not None and description is not None:
+                    the_recipe.name = name
+                    the_recipe.description = description
+                    db.session.add(the_recipe)
+                    db.session.commit()
+                    edit_response = {
+                        'status': 'Success',
+                        'message': 'Recipe details successfully edited'
+                    }
+
+                    return edit_response, 204
+                elif name is not None and description is None:
+                    the_recipe.name = name
+                    db.session.add(the_recipe)
+                    db.session.commit()
+                    edit_response = {
+                        'status': 'Success',
+                        'message': 'Recipe name successfully edited'
+                    }
+
+                    return edit_response, 204
+                elif name is None and description is not None:
+                    the_recipe.description = description
+                    db.session.add(the_recipe)
+                    db.session.commit()
+                    edit_response = {
+                        'status': 'Success',
+                        'message': 'Recipe description successfully edited'
+                    }
+
+                    return edit_response, 204
+                else:
+                    return {'message': 'No changes to be made'}
+                return {'message': 'The recipe does not exist'}
+        except Exception as e:
+
+            edit_response = {
+                'message': str(e)
+            }
+            traceback.print_exc()
+            return post_response
+
+    @api.response(204, 'Success')
+    def delete(self, name):
+        ''' This method deletes a Recipe '''
+        try:
+            the_recipe = Recipe.query.filter_by(name=name).first()
+            if the_recipe is not None:
+                db.session.delete(the_recipe)
+                db.session.commit()
+                return {'message': 'The recipe was successfully deleted'}
+            else:
+                return {'message': 'The recipe does not exist'}
+        except Exception as e:
+
+            post_response = {
+                'message': str(e)
+            }
+            traceback.print_exc()
+            return post_response
