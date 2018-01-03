0719b97cc8bdcc69e41a408ebc284ec15271c53d
Thegaijin <ssemagandavictor@gmail.com>
2018-01-03 20:34:23 +0300

    [ADD]: Add model for the blacklisted tokens table

---
+++ app/models/blacklist.py
@@ -0,0 +1,25 @@
+# app/models/blacklist.py
+''' This scripts holds the Blacklist model '''
+
+# Third party import
+from datetime import datetime
+
+# Local import
+from ..db import db
+
+
+class Blacklist(db.Model):
+    ''' Class representing the blacklisted table '''
+
+    __tablename__ = 'blacklisted'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    token = db.Column(db.String(500), unique=True, nullable=False)
+    blacklist_date = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, token):
+        self.token = token
+        self.blacklist_date = datetime.now()
+
+    def __repr__(self):
+        return '<id: token: {}'.format(self.token)
