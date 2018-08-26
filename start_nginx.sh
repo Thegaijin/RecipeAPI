d372c25d83dd6d234b6cd26b00dc301256d0bf7d
Thegaijin <ssemagandavictor@gmail.com>
2018-08-27 00:50:44 +0300

    Add docker compose to ci

--- start_nginx.sh
+++ start_nginx.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 service nginx start
-export FLASK_APP=manage.py
-export FLASK_CONFIG=development
-export SECRET_KEY='feagrhtjeyagdhjsyt'
-# POSTGRES_DB=recipe_db
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
+# export FLASK_APP=manage.py
+# export FLASK_CONFIG=development
+# export SECRET_KEY='feagrhtjeyagdhjsyt'
+# # POSTGRES_DB=recipe_db
+# export LC_ALL=C.UTF-8
+# export LANG=C.UTF-8
 # flask db init
 # flask db migrate
 # flask db upgrade
