839701276af43323b0d33560cce4af31daad7bdc
Thegaijin <ssemagandavictor@gmail.com>
2018-08-29 00:45:48 +0300

    attempt kubectl 2

--- k8s.sh
+++ k8s.sh
@@ -31,8 +31,8 @@ fi
 
 echo "checking for clusters"
 CLUSTER_NAMES="$(kops get clusters --state=s3://${BUCKET_NAME})"
-for name in $CLUSTER_NAMES; do
-  if [name == $CLUSTER_NAME ]; then
+for name in ${CLUSTER_NAMES}; do
+  if [ ${name} == ${CLUSTER_NAME} ]; then
     kubectl apply -f -R ~/project/k8s/
   else
     echo "There is no cluster. to deploy to.."
