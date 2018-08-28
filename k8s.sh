803bb82c002821e5ace5cde78684fb3545c0c654
Thegaijin <ssemagandavictor@gmail.com>
2018-08-28 16:52:35 +0300

    Add k8s file and deployment folder

---
+++ k8s.sh
@@ -0,0 +1,46 @@
+. /home/ubuntu/.env
+
+echo "Get s3 bucket..."
+# Check available buckets
+buckets="$(aws s3api list-buckets | jq -r '.Buckets')"
+found_bucket=false
+
+# check if bucket already exists
+for name in $( echo ${buckets} | jq -c '.[]'); do
+	bucket_names=$(echo ${name} | jq -r '.Name')
+	the_bucket=$(echo ${bucket_names} | grep ${BUCKET_NAME})
+	if [[ ${the_bucket} == ${BUCKET_NAME} ]]; then
+	found_bucket=true
+	break
+	fi
+done
+
+if [ ${found_bucket} == false ]; then
+	echo "Create the bucket..."
+	export BUCKET_NAME=$BUCKET_NAME
+	aws s3api create-bucket --bucket $BUCKET_NAME
+	export KOPS_STATE_STORE=s3://$BUCKET_NAME
+fi
+
+echo "Generate public key from pem file"
+chmod 400 /home/ubuntu/jenk8ns-key-pair.pem
+ssh-keygen -y -f /home/ubuntu/jenk8ns-key-pair.pem > /home/ubuntu/.ssh/id_rsa.pub
+
+echo "Creating cluster..."
+kops create cluster --dns-zone thegaijin.xyz --zones us-east-1a --master-size t2.micro --node-size t2.micro --name $CLUSTER_NAME --ssh-public-key /home/ubuntu/.ssh/id_rsa.pub --yes
+echo "************************ validate cluster **************************"
+while true; do
+  kops validate cluster --name $CLUSTER_NAME | grep 'is ready' &> /dev/null
+  if [ $? == 0 ]; then
+    break
+  fi
+    sleep 30
+done
+
+echo "<<<<<<<<<<<<< get the cluster >>>>>>>>>>>>>"
+kops get cluster
+kubectl cluster-info
+# kubectl apply namespace ingress
+
+echo "Add Dashboard"
+kubectl create -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.4.0.yaml
