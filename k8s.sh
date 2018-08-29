fa902a39d430d5e9d91d0ff70c7c73eae6fad33f
Thegaijin <ssemagandavictor@gmail.com>
2018-08-29 14:23:59 +0300

    Final deployment

--- k8s.sh
+++ k8s.sh
@@ -22,20 +22,9 @@ if [ ${found_bucket} == false ]; then
 	export KOPS_STATE_STORE=s3://${BUCKET_NAME}
 fi
 
-# echo "Generate public key from pem file"
-# echo ${PEM_FILE} > ~/project/key-pair.pem
-# ls -la
-# pwd
-# chmod 400 ~/project/key-pair.pem
-# ssh-keygen -y -f ~/project/key-pair.pem > ~/project/id_rsa.pub
-
 echo "checking for clusters"
 export FLASK_CONFIG="$(echo ${FLASK_CONFIG})"
 export SECRET_KEY="$(echo ${SECRET_KEY})"
-
-echo "This is the flask config $FLASK_CONFIG"
-echo "This is the secret key $SECRET_KEY"
-
 
 echo "Creating the deployment yml file"
 sudo cat <<EOF > ~/project/k8s/recipeapi_deployment.yml
@@ -88,39 +77,3 @@ for name in ${CLUSTER_NAMES}; do
     echo "There is no cluster to deploy to.."
   fi
 done
-
-
-# echo "These are the $CLUSTERS"
-# FOUND_CLUSTER=false
-# THE_CLUSTER=$(echo ${CLUSTERS} | grep ${CLUSTER_NAME})
-# if [[ ${THE_CLUSTER} == ${CLUSTER_NAME} ]]; then
-# 	FOUND_CLUSTER=true
-# 	break
-# fi
-
-
-# if [ $THE_CLUSTER == false ]; then
-# echo "There is no cluster..."
-# fi
-
-
-# if [ $THE_CLUSTER == false ]; then
-# 	echo "Creating cluster..."
-# 	kops create cluster --dns-zone thegaijin.xyz --zones us-east-1a --master-size t2.micro --node-size t2.micro --name $CLUSTER_NAME --ssh-public-key /home/ubuntu/.ssh/id_rsa.pub --yes
-# 	echo "************************ validate cluster **************************"
-# 	while true; do
-# 	kops validate cluster --name $CLUSTER_NAME | grep 'is ready' &> /dev/null
-# 	if [ $? == 0 ]; then
-# 		break
-# 	fi
-# 	  sleep 30
-# done
-# fi
-
-echo "<<<<<<<<<<<<< get the cluster >>>>>>>>>>>>>"
-# kops get clusters
-# kubectl cluster-info
-# kubectl apply namespace ingress
-
-echo "Add Dashboard"
-# kubectl create -f https://raw.githubusercontent.com/kubernetes/kops/master/addons/kubernetes-dashboard/v1.4.0.yaml
